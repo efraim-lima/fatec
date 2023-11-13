7705605455ab53286ccb022eb3faee0098bbfeb8
Efraim <efraim.alima@gmail.com>
2023-11-13 09:38:26 -0300

    Exercícios da atividade 2 concluídos

--- ATIVIDADEI/exercicio7.py
+++ ATIVIDADEI/exercicio7.py
@@ -5,7 +5,7 @@ if metrificacao == "C":
     temperatura2 = (temperatura * 1.8) + 32
     print(f"{temperatura:.1f} é equivalente a {temperatura2:.2f}")
 elif metrificacao == "F":
-    temperatura2 = (temperatura - 32) / 1,8
+    temperatura2 = (temperatura - 32) / 1.8
     print(f"{temperatura:.1f} é equivalente a {temperatura2:.2f}")
 else:
     print("não entendi algo, por favor, volte ao começo.")
